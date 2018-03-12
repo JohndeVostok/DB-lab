8398c31e74bd0981458ecf0408f364cb2316beb1
Zixuan <dyxdy@live.com>
2018-03-12 20:57:28 +0800

    create index

--- SimSearcher/SimSearcher.cpp
+++ SimSearcher/SimSearcher.cpp
@@ -1,5 +1,7 @@
 #include "SimSearcher.h"
 #include <cstdlib>
+#include <cstdio>
+#include <cmath>
 
 using namespace std;
 
@@ -13,14 +15,42 @@ SimSearcher::~SimSearcher()
 
 int SimSearcher::createIndex(const char *filename, unsigned q)
 {
+	mp.clear();
+
+	FILE *pFile = fopen ("myfile.txt","w");
+	setvbuf(pFile, NULL, _IOFBF, 0xfffff);
 	char buf[1024];
-	FILE* 
+	int index = 0;
+	data.clear();
+	while (fscanf(pFile, "%s", buf) != -1) {
+		data.push_back(buf);
+		int len = strlen(buf);
+		for (int i = 0; i <= len - q; i++) {
+			std::string tmp(buf + i, q);
+			if (!mp.count(tmp)) {
+				mp[tmp] = std::vector(1, index);
+			} else {
+				mp[tmp].push_back(index);
+			}
+		}
+		index++;
+	}
+	fclose(pFile);
 	return SUCCESS;
 }
 
 int SimSearcher::searchJaccard(const char *query, double threshold, vector<pair<unsigned, double> > &result)
 {
 	result.clear();
+	gram.clear();
+	int len = ceil(strlen(query) - q + 1 - threshold * q);
+	for (int i = 0; i <= len - q; i++) {
+		std::string tmp(buf + i, q);
+		if (mp.count(tmp)) {
+			gram.push_back(&mp[tmp]);
+		}
+	}
+	
 	return SUCCESS;
 }
 
--- SimSearcher/SimSearcher.h
+++ SimSearcher/SimSearcher.h
@@ -1,12 +1,19 @@
 #pragma once
+#include <queue>
 #include <vector>
+#include <string>
 #include <utility>
+#include <unordered_map>
 
 const int SUCCESS = 0;
 const int FAILURE = 1;
 
 class SimSearcher
 {
+private:
+	std::unordered_map <std::string, std::vector<int>> mp;
+	std::vector <std::string> data;
+	std::vector <std::vector<int>*> gram;
 public:
 	SimSearcher();
 	~SimSearcher();
