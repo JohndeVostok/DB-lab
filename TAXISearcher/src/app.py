9ff350a37fce1d21e0f1e85261499316f576bf6f
mazx <dyxdy@live.com>
2018-05-14 17:06:46 +0800

    tmp work

--- TAXISearcher/src/app.py
+++ TAXISearcher/src/app.py
@@ -1,8 +1,11 @@
+import pickle
 import wx
+
+scale = 200
 
 class MapUI(wx.Frame):
 	def __init__(self, parent, title):
-		super(MapUI, self).__init__(parent, title = title, size = (800, 600))
+		super(MapUI, self).__init__(parent, title = title, size = (950, 800))
 		self.InitUI()
 	def InitUI(self):
 		self.Bind(wx.EVT_PAINT, self.OnPaint)
@@ -13,7 +16,22 @@ class MapUI(wx.Frame):
 		brush = wx.Brush("white")
 		dc.SetBackground(brush)
 		dc.Clear()
-		dc.DrawLine(0, 0, 800, 600)
+
+		pen = wx.Pen(wx.Colour(0,0,0))
+		dc.SetPen(pen)
+		brush = wx.Brush("red")
+		for x in range(0, 950, 50):
+			dc.DrawLine(x, 0, x, 800)
+		for y in range(0, 800, 50):
+			dc.DrawLine(0, y, 950, y)
+
+		pen = wx.Pen(wx.Colour(0,0,255))
+		dc.SetPen(pen)
+		for edge in edges:
+			node0 = nodes[edge[0]]
+			node1 = nodes[edge[1]]
+			dc.DrawLine(node0[0] / scale, node0[1] / scale, node1[0] / scale, node1[1] / scale)
+
 
 #		dc.DrawBitmap(wx.Bitmap("python.jpg"),10,10,True) 
 #		color = wx.Colour(255,0,0)
@@ -31,7 +49,10 @@ class MapUI(wx.Frame):
 #		dc.SetBrush(wx.Brush(wx.Colour(0,255,0), wx.CROSS_HATCH)) 
 #		dc.DrawRectangle(380, 15, 90, 60)
 
-app = wx.App()
-MapUI(None, 'TAXI Searcher')
-app.MainLoop()
+if __name__ == "__main__":
+	with open("../data/map.pkl", "rb") as f:
+		(nodes, edges) = pickle.load(f)
+	app = wx.App()
+	MapUI(None, 'TAXI Searcher')
+	app.MainLoop()
 
--- TAXISearcher/src/net.py
+++ TAXISearcher/src/net.py
@@ -1,3 +1,5 @@
+import pickle
+
 R = 75000
 grid = 10000
 
@@ -8,29 +10,33 @@ def dist(ida, idb):
 	return tmp
 
 if __name__ == "__main__":
-    with open("../data/road.cnode") as f:
-        lines = f.readlines()
-    nodes = []
-    for line in lines:
-        tmp = line.split(" ")
-        nodes.append([float(tmp[1]) * R, float(tmp[2]) * R])
-    with open("../data/road.nedge") as f:
-    	lines = f.readlines()
-    lines.pop(0)
-#    print(mapWidth - max([tmp[0] for tmp in nodes]), min([tmp[0] for tmp in nodes]))
-#    print(mapHeight - max([tmp[1] for tmp in nodes]), min([tmp[1] for tmp in nodes]))
+	with open("../data/road.cnode") as f:
+		lines = f.readlines()
+	nodes = []
+	for line in lines:
+		tmp = line.split(" ")
+		nodes.append([float(tmp[1]) * R, float(tmp[2]) * R])
+	with open("../data/road.nedge") as f:
+		lines = f.readlines()
+	lines.pop(0)
+#	print(mapWidth - max([tmp[0] for tmp in nodes]), min([tmp[0] for tmp in nodes]))
+#	print(mapHeight - max([tmp[1] for tmp in nodes]), min([tmp[1] for tmp in nodes]))
 
-    mapxMin = int((min([tmp[0] for tmp in nodes]) / 10000) - 1) * 10000
-    mapxMax = int((max([tmp[0] for tmp in nodes]) / 10000) + 2) * 10000
-    mapyMin = int((min([tmp[1] for tmp in nodes]) / 10000) - 1) * 10000
-    mapyMax = int((max([tmp[1] for tmp in nodes]) / 10000) + 2) * 10000
-    mapWidth = mapxMax - mapxMin
-    mapHeight = mapyMax - mapyMin
+	mapxMin = int((min([tmp[0] for tmp in nodes]) / grid) - 1) * grid
+	mapxMax = int((max([tmp[0] for tmp in nodes]) / grid) + 2) * grid
+	mapyMin = int((min([tmp[1] for tmp in nodes]) / grid) - 1) * grid
+	mapyMax = int((max([tmp[1] for tmp in nodes]) / grid) + 2) * grid
+	mapWidth = mapxMax - mapxMin
+	mapHeight = mapyMax - mapyMin
 
-    print(mapWidth, mapHeight)
+	for i in range(len(nodes)):
+		nodes[i][0] -= mapxMin
+		nodes[i][1] -= mapyMin
 
-    edges = []
-    for line in lines:
-    	tmp = line.split(" ")
-    	edges.append([int(tmp[0]), int(tmp[1]), int(tmp[2])])
-    cnt = 0
+	edges = []
+	for line in lines:
+		tmp = line.split(" ")
+		edges.append([int(tmp[0]), int(tmp[1]), int(tmp[2])])
+
+	with open("../data/map.pkl", "wb") as f:
+		pickle.dump((nodes, edges), f)
